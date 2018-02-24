442a643017e811b33d656b8409c332607d53ceb9
Teraleon <nikleon@gmail.com>
2018-02-24 08:47:27 -0800

    Added tabs and a login grid

--- src/java/mem/MEM.java
+++ src/java/mem/MEM.java
@@ -2,18 +2,20 @@ package mem;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class MEM extends Application {
-
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		BorderPane root = FXMLLoader.load(getClass().getClassLoader().getResource("GUI.fxml"));
+		Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("GUI.fxml"));
 
 		primaryStage.setScene(new Scene(root));
 		primaryStage.setTitle("Multi-tasking Enhancement Modules");
+		primaryStage.initStyle(StageStyle.DECORATED);
+		primaryStage.setResizable(false);
 		primaryStage.show();
 	}
 
